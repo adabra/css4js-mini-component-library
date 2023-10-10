88628b6aa7a11bdccc91fb8382424a82afcdf0ff
adabra <audundb@gmail.com>
2023-10-10 11:18:05 +0200

    Implement Select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,47 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper value={value} onChange={onChange}>
+      {displayedValue}<SelectIcon id="chevron-down" strokeWidth="2" size="24px"></SelectIcon>
+      <HiddenNativeSelect>{children}</HiddenNativeSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  border: none;
+  appearance: none;
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+  padding-right: 10px;
+  color: ${COLORS.gray700};
+  border-radius: 8px;
+  width: max-content;
+  position: relative;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+
+  &:focus-within {
+    outline: auto;
+  }
+`;
+
+const SelectIcon = styled(Icon)`
+  display: inline-block;
+  vertical-align: middle;
+  margin-left: 18px;
+`;
+
+const HiddenNativeSelect = styled.select`
+  opacity: 0;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  appearance: none;
+`;
 
 export default Select;
